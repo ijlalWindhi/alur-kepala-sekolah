3f3586aad24552bde5e62b62bfd494435c36255d
Indahs11 <indahsrw11@gmail.com>
2022-03-25 18:59:09 +0700

    beranda

--- src/components/Layout/LayoutSidebar.js
+++ src/components/Layout/LayoutSidebar.js
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
+import UserLog from '../../assets/user-login.png'
+import Logo from '../../assets/logo.png'
 
 export default function LayoutSidebar({ children }) {
   const sidebarContent = [
@@ -27,29 +29,27 @@ export default function LayoutSidebar({ children }) {
       label: "Forum Diskusi",
       target: "/forumDiskusi",
     },
-    {
-      label: "Kembali ke Landing Page",
-      target: "/landingPage",
-    },
   ];
 
   return (
     <div className="parent md:h-screen md:grid md:grid-cols-6">
       <section className="sidebar bg-green-400 md:col-span-1">
         {/* LOGO DISINI */}
-        <div className="box-icon">PLATFORM SMK TKJ</div>
+        <div className="box-icon text-sm font-semibold justify-center items-center">
+          <img src={Logo}/>PLATFORM SMK TKJ
+          </div>
         <div>
           <ul>
             {/* NAV-LINK */}
             {sidebarContent.map(({ target, label }, i) => (
-              <li>
-                <Link to={target} className="flex items-center" aria-current="page">
-                  icon
+              <li className="pl-6">
+                <Link to={target} className="flex items-center p-4 text-base font-normal" aria-current="page">
+                  
                   {label}
                 </Link>
               </li>
             ))}
-            <li>akun yg login</li>
+            <li className="absolute bottom-14 left-6 text-lg font-medium"><img src={UserLog} width="42" className="inline pr-2"/>Jhon Doe</li>
           </ul>
         </div>
       </section>
--- src/pages/Beranda/Beranda.js
+++ src/pages/Beranda/Beranda.js
@@ -3,6 +3,8 @@ import React from "react";
 
 // inisiasi component
 import LayoutSidebar from "../../components/Layout/LayoutSidebar";
+import CardBox from "../../components/fragment/Beranda/CardBox";
+import UserLogin from "../../assets/user-login.png"
 
 export default class Beranda extends React.Component {
   constructor() {
@@ -15,7 +17,24 @@ export default class Beranda extends React.Component {
     return (
       <>
         <LayoutSidebar>
-          <h1>ini beranda</h1>
+          <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3 mt-8">
+            <div class="col-span-2 ml-14">
+              <div className="grid grid-cols-4">
+                <CardBox/>
+                <CardBox/>
+                <CardBox/>
+                <CardBox/>
+              </div>
+              <div className="card rounded-lg bg-white p-6 mt-8">
+                <img src={UserLogin} className="inline"/>
+                <input type="text" className=" w-4/5 h-12 bg-slate-100 rounded-full px-6 ml-6" placeholder="Apa yang anda pikirkan sekarang?"></input>
+              </div>
+            </div>
+            <div>03</div>
+            <div>04</div>
+            <div>05</div>
+          </div>
+          
         </LayoutSidebar>
       </>
     );
