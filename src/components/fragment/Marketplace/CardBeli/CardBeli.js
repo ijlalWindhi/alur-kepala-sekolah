7274f0cdf4053abc407b26ea4d86f79abcb90878
Indahs11 <indahsrw11@gmail.com>
2022-03-27 10:41:29 +0700

    marketplace

---
+++ src/components/fragment/Marketplace/CardBeli/CardBeli.js
@@ -0,0 +1,35 @@
+// inisasi library default
+import React, { useState, useEffect } from "react";
+// inisiasi component
+import Item from "../../../../assets/item.svg"
+
+export default function CardBeli({ handleAction }) {
+  return (
+    <div class="p-6 card bg-white rounded-3xl">
+        <div className="grid grid-cols-5 gap-4">
+            <div className="">
+                <img className="" src={Item}></img>
+            </div>
+            <div className="col-span-3">
+                <h5 className="text-base font-medium ">Intel Core</h5>
+                <span className="text-xs font-normal text-gray-400">INV/BUY/11928410</span>
+                <h6 className="font-normal text-sm text-grey-700 mt-2">Rp.8797192379</h6>
+                <p className="font-normal text-sm text-red-600">Cyber Mall</p>
+            </div>
+            <div>
+                <h5>12 Maret 2021</h5>
+                <div className="mt-4">
+                    <button className="btn btn-md bg-red-600 py-2 px-6 rounded-full text-white mt-6">Detail</button>
+                </div>
+            </div>
+        </div>
+        {/* <div className="flex justify-center">
+            <img className="" src={Item}></img>
+        </div>
+        <h5 className="mb-2 text-base font-medium mt-10">Intel Core</h5>
+        <p className="font-normal text-sm text-red-600">Kota Malang</p>
+        <h6 className="font-normal text-sm text-grey-700 mt-2">Rp.8797192379</h6>
+        <button className="btn btn-md bg-red-600 py-2 px-6 rounded-full text-white mt-6">Beli</button> */}
+    </div>
+  );
+}
--- src/pages/Beranda/Marketplace/Marketplace.js
+++ src/pages/Beranda/Marketplace/Marketplace.js
@@ -4,6 +4,8 @@ import React from "react";
 // inisiasi component
 import LayoutSidebar from "../../../components/Layout/LayoutSidebar";
 import ItemCard from "../../../components/fragment/Marketplace/ItemCard";
+import CardBeli from "../../../components/fragment/Marketplace/CardBeli";
+import SummaryCard from "../../../components/fragment/Marketplace/SummaryCard";
 
 export default class Marketplace extends React.Component {
   constructor() {
@@ -18,36 +20,78 @@ export default class Marketplace extends React.Component {
         <LayoutSidebar>
           <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3 mt-10">
             <div class="col-span-2 ml-14">
-              <h1 className="mb-8 font-semibold text-xl">Pilihan Hari ini</h1>
-              <div className="grid grid-cols-3 gap-4">
-                <ItemCard/>
-                <ItemCard/>
-                <ItemCard/>
+              <div id="tabContent">
+                  {/* Tab Content 1 */}
+                  <div class="hidden p-4" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                    <div className="mb-8 grid grid-cols-4">
+                      <h1 className="col-span-3 font-semibold text-xl inline">Pembelian Barang</h1>
+                      <span className="col-span-1 text-right text-base font-normal"> Jakarta</span>
+                    </div>
+                    <div className="grid grid-cols-3 gap-4">
+                      <ItemCard/>
+                      <ItemCard/>
+                      <ItemCard/>
+                    </div>
+                  </div>
+                  {/* Tab Content 2 */}
+                  <div class="hidden p-4" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+                    <div className="mb-8 grid grid-cols-4">
+                      <h1 className="col-span-3 font-semibold text-xl inline">Pembelian Barang</h1>
+                      <span className="col-span-1 text-right text-base font-normal text-red-600"> Beli Barang</span>
+                    </div>
+                    <div className="grid cols-5 gap-4">
+                      <CardBeli/>
+                      <CardBeli/>
+                      <CardBeli/>
+                    </div>
+                  </div>
+                  {/* Tab Content 3 */}
+                  <div class="hidden p-4" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+                    <div className="mb-8 grid grid-cols-4">
+                      <h1 className="col-span-3 font-semibold text-xl inline">Penjualan Barang</h1>
+                      <span className="col-span-1 text-right text-base font-normal text-red-600"> Jual Barang</span>
+                    </div>
+                    <div className="grid grid-cols-3 gap-4">
+                      <SummaryCard/>
+                      <SummaryCard/>
+                      <SummaryCard/>
+                    </div>
+                  </div>
               </div>
             </div>
             <div>
-              <div className="pl-32">
-                <h4 className="text-lg font-semibold">Kontak</h4>
-                <div className="mt-6">
-                  <img src="" className="inline w-10 mr-4"></img>
-                  <span className="text-base font-normal">Alfia Zena</span>
+              <div className="pl-11 pr-14">
+                <h4 className="text-lg font-semibold">Marketplace</h4>
+                <input type="text" className="rounded-full w-80 h-11 px-8 mt-8  border-red-400" placeholder="Search" style={{border: `1px solid #E70000`}}></input>
+                <div>
+                <div class="mb-4 border-b border-gray-200 mt-8">
+                  <ul class=" -mb-px text-sm font-medium" id="tab" data-tabs-toggle="#tabContent" role="tablist">
+                    <li class="mr-2" role="presentation">
+                        <button class="inline-block p-4 rounded-t-lg border-b-2" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Telusuri Semua</button>
+                    </li>
+                    <li class="mr-2" role="presentation">
+                        <button class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Beli</button>
+                    </li>
+                    <li class="mr-2" role="presentation">
+                        <button class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Jual</button>
+                    </li>
+                  </ul>
                 </div>
-                <div className="mt-6">
-                  <img src="" className="inline w-10 mr-4"></img>
-                  <span className="text-base font-normal">Alfia Zena</span>
                 </div>
-                <div className="mt-6">
-                  <img src="" className="inline w-10 mr-4"></img>
-                  <span className="text-base font-normal">Alfia Zena</span>
+                <div className="mt-5">
+                  <h4 className="text-lg font-semibold">Filter</h4>
+                  <div className="">
+                    <span className="">Jakarta, Indonesia</span>
+                  </div>
                 </div>
-              </div>
-              <div className="pl-32 mt-12">
-                <h4 className="text-lg font-semibold">Percakapan Grub</h4>
-                <div className="mt-6">
-                  <a href="#">
-                    <img src="" className="inline mr-4"></img>
-                    <span className="font-normal text-base text-slate-400">Buat grup baru</span>
-                  </a>
+                <div className="mt-5">
+                  <h4 className="text-lg font-semibold">Kategori</h4>
+                  <div className="flex flex-wrap gap-2">
+                    <span className="badge bg-red-200 p-4 rounded-3xl text-xs font-medium text-red-600">Component</span>
+                    <span className="badge bg-red-200 p-4 rounded-3xl text-xs font-medium text-red-600">Aksesoris Komputer</span>
+                    <span className="badge bg-red-200 p-4 rounded-3xl text-xs font-medium text-red-600">Komputer</span>
+                    <span className="badge bg-red-200 p-4 rounded-3xl text-xs font-medium text-red-600">Komputer</span>
+                  </div>
                 </div>
               </div>
             </div>
