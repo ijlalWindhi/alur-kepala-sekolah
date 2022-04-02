c160a80095645ff0146215225bb1bf7bffcc172c
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-04-02 10:58:41 +0700

    style beranda

--- src/components/Layout/LayoutSidebar.js
+++ src/components/Layout/LayoutSidebar.js
@@ -4,30 +4,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 import UserLog from '../../assets/user-login.png'
 import Logo from '../../assets/logo.png'
+import Bell from '../../assets/bell.svg'
+import Power from '../../assets/power.svg'
+import Search from '../../assets/icon-search.svg'
+import Beranda from '../../assets/icon-beranda.svg'
+import Blog from '../../assets/icon-blog.svg'
+import Marketplace from '../../assets/icon-marketplace.svg'
+import Pengajuan from '../../assets/icon-pengajuan.svg'
 
 export default function LayoutSidebar({ children }) {
   const sidebarContent = [
     {
       label: "Dashboard",
       target: "/beranda",
-      // icon: "icon/estate.svg",
+      icon: Beranda,
     },
 
     {
       label: "Blog/Berita",
       target: "/blog",
+      icon: Blog,
     },
     {
       label: "Markerplace",
       target: "/marketplace",
+      icon: Marketplace,
     },
     {
       label: "Pengajuan",
       target: "/pengajuan",
+      icon: Pengajuan,
     },
     {
       label: "Forum Diskusi",
       target: "/forumDiskusi",
+      icon: Beranda,
     },
   ];
 
@@ -44,15 +55,14 @@ export default function LayoutSidebar({ children }) {
         <div>
           <ul>
             {/* NAV-LINK */}
-            {sidebarContent.map(({ target, label }, i) => (
-              <li className="pl-6">
+            {sidebarContent.map(({ target, label, icon }, i) => (
+              <li className="ml-3 pl-6 hover:bg-red-500 hover:text-white hover:rounded-full w-9/12">
                 <Link to={target} className="flex items-center p-4 text-base font-normal md:dark:hover:text-white dark:hover:bg-green-700 dark:hover:text-white dark:border-gray-700" aria-current="page">
-                  
-                  {label}
+                <img src={icon} className="mr-3"/> {label}
                 </Link>
               </li>
             ))}
-            <li className="pl-6">
+            <li className="ml-3 pl-6">
               <button id="dropdownDefault" data-dropdown-toggle="dropdown" className="text-dark border-red-400 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">Pengajuan<svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
                   <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
                       <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
@@ -88,14 +98,25 @@ export default function LayoutSidebar({ children }) {
       </section>
       <main class="main md:col-span-5 bg-gray-100">
         <div className="grid grid-cols-4 gap-4 pt-12 ml-14">
-          <div className="col-span-3">
-            <input type="text" className="rounded-full w-80 h-11 px-8 border-red-400" placeholder="Search" style={{border: `1px solid #E70000`}}></input>
+          {/* <div className="col-span-3 relative">
+            <input type="text" className="rounded-full w-80 h-11 px-8 border-none" placeholder="Search"></input>
+            <button type="submit" className="absolute">
+              <img src={Search}/>
+            </button>
+          </div> */}
+          <div class="col-span-3 relative">
+            <input class="focus:ring-red-500 focus:border-red-500 rounded-full w-80 h-11 px-8 border-none" text-black type="text" name="search" placeholder="Search"></input>
+            <button type="submit" class="absolute left-0 top-0 ml-64">
+              <img src={Search}/>
+            </button>
           </div>
           <div className="mt-2">
-            <span className="badge bg-red-300 rounded-full pl-2 pr-4 pt-2 pb-3">
+            <span className="badge bg-red-300 rounded-full pl-2 pr-4 pt-2 pb-3 cursor-pointer">
               <img src={UserLog} className="inline w-8 mr-2"></img>
               <span className="text-sm font-medium text-red-500">John Doe</span>
             </span>
+            <img src={Bell} className="inline w-8 ml-8 mr-8 cursor-pointer"></img>
+            <img src={Power} className="inline w-8 cursor-pointer"></img>
           </div>
         </div>
         {/* <!-- Page content--> */}{" "}
