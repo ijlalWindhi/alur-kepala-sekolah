33df620b6e94267db2d812bdc01bbfa616055d52
Indahs11 <indahsrw11@gmail.com>
2022-04-16 14:26:39 +0700

    penyisipan tugas

--- src/pages/Beranda/Lms/Siswa/Kelas/DetailTugas/DetailTugas.js
+++ src/pages/Beranda/Lms/Siswa/Kelas/DetailTugas/DetailTugas.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Book, Clock, File } from 'react-feather';
+import './DetailTugas.css'
+import { Book, Clock, File, Save } from 'react-feather';
 import { Link } from "react-router-dom";
 
 import LayoutSidebar from "../../../../../../components/Layout/LayoutSidebar";
@@ -41,9 +42,25 @@ export default class DetailTugasSiswa extends React.Component{
                                     <div className="grid grid-cols-3 mt-4 gap-6">
                                         <div className="col-span-2 pr-4">
                                           <p className="text-sm font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida dictumst vivamus egestas. Quam semper tortor sit vestibulum sit gravida odio montes, vitae. Lobortis dis egestas suspendisse consectetur non metus, vestibulum. Morbi ut pulvinar dictum suscipit montes, congue. Duis sit magna a amet dolor. Nisl maecenas a quis faucibus sed</p>
+                                          <div className="preview-file bg-zinc-100 rounded-3xl p-3 grid grid-cols-4 gap-4 mt-4">
+                                            <img src="https://4.bp.blogspot.com/-hvL0TPG2C84/WBQ53QeiAtI/AAAAAAAAAFM/2HxtHqWzBzIOJ9k3eweJihk55hEDKjtFgCLcB/s1600/3.jpg" className="w-16 h-16 rounded-lg"></img>
+                                            <div className="col-span-3">
+                                                <a href="http://africau.edu/images/default/sample.pdf" className="text-sm font-medium">Latihan Soal USP</a>
+                                                <h6 className="text-xs font-normal">.docx</h6>
+                                            </div>
+                                          </div>
+                                            {/* <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="100%">
+                                                <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+                                            </object> */}
                                         </div>
                                         <div className="">
                                             <h1 className="text-base font-medium">Pengumpulan Tugas</h1>
+                                            <label className="bg-zinc-100 rounded-lg text-center px-8 py-6 custom-file-upload mt-4">
+                                                <Save className="text-red-600 m-2 flex justify-items-center" size={24}/>
+                                                <h4 className="text-red-600 my-2 text-sm font-normal">Tarik dokumen</h4>
+                                                <input type="file"/>
+                                                <div className="bg-red-600 text-white p-2 rounded-full text-xs ">Tambah Dokumen</div>
+                                            </label>
                                         </div>
                                     </div>
                                 </div>
