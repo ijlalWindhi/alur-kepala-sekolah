1988c7b4f8f03da286d46516d4e1fade5291aa0a
ajinata84 <ajinata89@gmail.com>
2022-05-03 22:05:33 +0700

    pengajuan telkom group

--- src/pages/Beranda/Pengajuan/GuruTamu/GuruTamu.js
+++ src/pages/Beranda/Pengajuan/GuruTamu/GuruTamu.js
@@ -465,7 +465,7 @@ export default class GuruTamu extends React.Component {
               <div class="relative overflow-x-auto shadow-md ">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                   <thead
-                    class="text-white  text-center"
+                    class="text-white  text-start"
                     style={{ background: "#474747" }}
                   >
                     <tr>
@@ -489,9 +489,6 @@ export default class GuruTamu extends React.Component {
                       </th>
                       <th scope="col" class="px-6 font-medium py-3">
                         Opsi
-                      </th>
-                      <th scope="col" class="px-6 font-medium py-3">
-                        Status
                       </th>
                     </tr>
                   </thead>
@@ -525,54 +522,25 @@ export default class GuruTamu extends React.Component {
                         <td class="px-6 py-4">
                           <div className="flex flex-row">
                             <button
-                              className="font-small p-2 text-white dark:text-white  hover:underline ml-2 inline"
+                              className="font-small p-3 text-white dark:text-white  hover:underline ml-2 inline w-20"
                               data-modal-toggle="popup-delete"
-                              onClick={() => this.Delete(item)}
                               style={{
-                                background: "#E7008A",
-                                borderRadius: "8px",
+                                background: "#00DDB5",
+                                borderRadius: 15,
                               }}
                             >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                                />
-                              </svg>
+                              Approve
                             </button>
                             {/* {this.state.guest.map(item => ( */}
                             <button
-                              className="font-small p-2 text-white dark:text-white  hover:underline inline ml-2"
+                              className="font-small p-3 text-white dark:text-white  hover:underline inline ml-2 w-20"
                               data-modal-toggle="edit-modal"
-                              onClick={() => this.Edit(item)}
                               style={{
-                                background: "#58ADFC",
-                                borderRadius: "8px",
+                                background: "#E70000",
+                                borderRadius: 15,
                               }}
                             >
-                              {/*onClick={() => this.editGuest(item)}*/}
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                                />
-                              </svg>
+                              Decline
                             </button>
                           </div>
                           {/* ))} */}
@@ -858,9 +826,6 @@ export default class GuruTamu extends React.Component {
                               </div>
                             </>
                           ) : null}
-                        </td>
-                        <td className="px-6 py-4 text-black font-medium text-sm">
-                          {this.approvedPills(item.approved)}
                         </td>
                       </tr>
                     ))}
